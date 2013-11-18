6a07249a6177fa703200be688c5fc36432110325
oncletom <thomas@oncle-tom.net>
2013-11-18 23:56:30 +0000

    Implementing and testing `getFormData`

--- test/unit/core.js
+++ test/unit/core.js
@@ -2,20 +2,32 @@
 
 var Mailto = require('../../index.js');
 var expect = require('chai').expect;
+var fixtures;
+
+function createFixtures(){
+  fixtures = document.createElement('div');
+  fixtures.id = 'fixtures';
+
+  Object.keys(window.__html__).forEach(function(location){
+    location.match(/(\w+).html$/);
+
+    var el = document.createElement('div');
+    el.id = 'fixtures-'+RegExp.$1;
+    el.innerHTML = window.__html__[location];
+
+    fixtures.appendChild(el);
+  });
+
+  document.body.appendChild(fixtures);
+}
+
+function clearFixtures(){
+  document.body.removeChild(fixtures);
+}
 
 describe('Mailto Constructor', function(){
-  var fixtures;
-
-  before(function(){
-    fixtures = document.createElement('div');
-    fixtures.id = 'fixtures';
-    fixtures.innerHTML = window.__html__['test/fixtures/default.html'];
-    document.body.appendChild(fixtures);
-  });
-
-  after(function(){
-    document.body.removeChild(fixtures);
-  });
+  before(createFixtures);
+  after(clearFixtures);
 
   it('should accept a valid CSS selector string', function(){
     expect(new Mailto('.mailto-form')).to.be.an.instanceof(Mailto);
@@ -49,5 +61,19 @@ describe('Mailto Constructor', function(){
     // formatter
     expect(new Mailto(form, { formatter: function(data){ return 'hey'+data; } })).to.satisfy(function(m){ return m.formatter('hey') === 'heyhey'; });
     expect(new Mailto(form, { formatter: '' }).formatter).to.be.a('function');
+  });
+});
+
+describe('Mailto.getFormData()', function(){
+  before(createFixtures);
+  after(clearFixtures);
+
+  it('should collect default form data', function(){
+    var m = new Mailto('#fixtures-default .mailto-form');
+
+    expect(m.getFormData()).to.have.lengthOf(3);
+    expect(m.getFormData()[0]).to.deep.eq({ name: 'from', label: 'User Email', value: '' });
+    expect(m.getFormData()[1]).to.deep.eq({ name: 'subject', label: '', value: 'Default value' });
+    expect(m.getFormData()[2]).to.deep.eq({ name: 'message', label: 'Message Content:', value: '' });
   });
 });
